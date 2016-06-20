7e82efa62508f9df9cf74292b20dfb8a5bb5db46
Steven K Word <stevenword@gmail.com>
2016-06-20 15:24:48 -0700

    OOPify the plugin loader

--- index.php
+++ index.php
@@ -8,21 +8,10 @@ Version: 1.0.0
 Author URI: http://wpengine.com
  */
 
+// Exit if this file is directly accessed
+if ( ! defined( 'ABSPATH' ) ) exit;
+
 require_once( __DIR__ . '/vendor/autoload.php' );
-
-// Build our tools page.
-add_action( 'admin_menu', 'wpephpcompat_create_menu' );
-
-// Load our JavaScript.
-add_action( 'admin_enqueue_scripts', 'wpephpcompat_enqueue' );
-
-// The action to run the compatibility test.
-add_action( 'wp_ajax_wpephpcompat_start_test', 'wpephpcompat_start_test' );
-add_action( 'wp_ajax_wpephpcompat_check_status', 'wpephpcompat_check_status' );
-add_action( 'wpephpcompat_start_test_cron', 'wpephpcompat_start_test' );
-
-// Create custom post type.
-add_action( 'init', 'wpephpcompat_create_job_queue' );
 
 // Add the phpcompat WP-CLI command.
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
@@ -31,206 +20,249 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 
 /**
  * Summary.
- *
- * Description.
- *
- * @since  1.0.0
- * @return [type] [description]
  */
-function wpephpcompat_start_test() {
-	global $wpdb;
+class WPEngine_PHPCompat {
 
-	$wpephpc = new \WPEPHPCompat( __DIR__ );
+	/* Define and register singleton */
+	private static $instance = false;
 
-	if ( isset( $_POST['startScan'] ) ) {
-		$test_version = sanitize_text_field( $_POST['test_version'] );
-		$only_active = sanitize_text_field( $_POST['only_active'] );
-
-		$wpephpc->test_version = $test_version;
-
-		$wpephpc->only_active = $only_active;
-
-		$wpephpc->clean_after_scan();
+	/**
+	 * Summary.
+	 *
+	 * @return [type] [description]
+	 */
+	public static function instance() {
+		if( ! self::$instance ) {
+			self::$instance = new self;
+			self::$instance->init();
+		}
+		return self::$instance;
 	}
 
-	echo esc_html( $wpephpc->start_test() );
-	wp_die();
-}
+	/**
+	 * Initialize hooks and setup environment variables.
+	 *
+	 * @since 0.1.0
+	 */
+	public static function init() {
 
+		// Build our tools page.
+		add_action( 'admin_menu', array( self::instance(), 'create_menu' ) );
 
-/**
- * Summary.
- *
- * Description.
- *
- * @todo Use heartbeat API.
- *
- * @since  1.0.0
- * @return [type] [description]
- */
-function wpephpcompat_check_status() {
-	$scan_status = get_option( 'wpephpcompat.status' );
-	$count_jobs = wp_count_posts( 'wpephpcompat_jobs' );
-	$total_jobs = get_option( 'wpephpcompat.numdirs' );
+		// Load our JavaScript.
+		add_action( 'admin_enqueue_scripts', array( self::instance(), 'admin_enqueue' ) );
 
-	$to_encode = array(
-		'status'   => $scan_status,
-		'count'    => $count_jobs->publish,
-		'total'    => $total_jobs,
-		'progress' => 100 - ( ( $count_jobs->publish / $total_jobs ) * 100 )
-	);
+		// The action to run the compatibility test.
+		add_action( 'wp_ajax_wpephpcompat_start_test', array( self::instance(), 'start_test' ) );
+		add_action( 'wp_ajax_wpephpcompat_check_status', array( self::instance(), 'check_status' ) );
+		add_action( 'wpephpcompat_start_test_cron', array( self::instance(), 'start_test' ) );
 
-	// If the scan is still running.
-	if ( $scan_status ) {
-		$to_encode['results'] = '0';
-	} else {
-		// Else return the results and clean up!
-		$scan_results = get_option( 'wpephpcompat.scan_results' );
-		$to_encode['results'] = esc_html( $scan_results );
+		// Create custom post type.
+		add_action( 'init', array( self::instance(), 'create_job_queue' ) );
+	}
+
+	/**
+	 * Summary.
+	 *
+	 * Description.
+	 *
+	 * @since  1.0.0
+	 * @action wp_ajax_wpephpcompat_start_test
+	 * @action wpephpcompat_start_test_cron
+	 * @return [type] [description]
+	 */
+	function start_test() {
+		global $wpdb;
 
 		$wpephpc = new \WPEPHPCompat( __DIR__ );
-		$wpephpc->clean_after_scan();
+
+		if ( isset( $_POST['startScan'] ) ) {
+			$test_version = sanitize_text_field( $_POST['test_version'] );
+			$only_active = sanitize_text_field( $_POST['only_active'] );
+
+			$wpephpc->test_version = $test_version;
+			$wpephpc->only_active = $only_active;
+			$wpephpc->clean_after_scan();
+		}
+
+		echo esc_html( $wpephpc->start_test() );
+		wp_die();
 	}
 
-	echo json_encode( $to_encode );
-	wp_die();
+	/**
+	 * Check the progress or result of the tests.
+	 *
+	 * @todo Use heartbeat API.
+	 * @since  1.0.0
+	 * @action wp_ajax_wpephpcompat_check_status
+	 * @return null
+	 */
+	function check_status() {
+		$scan_status = get_option( 'wpephpcompat.status' );
+		$count_jobs = wp_count_posts( 'wpephpcompat_jobs' );
+		$total_jobs = get_option( 'wpephpcompat.numdirs' );
 
-}
+		$to_encode = array(
+			'status'   => $scan_status,
+			'count'    => $count_jobs->publish,
+			'total'    => $total_jobs,
+			'progress' => 100 - ( ( $count_jobs->publish / $total_jobs ) * 100 )
+		);
 
-/**
- * Create custom post type to store the directories we need to process.
- *
- * @since 1.0.0
- * @return  null
- */
-function wpephpcompat_create_job_queue() {
-	register_post_type( 'wpephpcompat_jobs',
-		array(
-			'labels' => array(
-				'name' => __( 'Jobs' ),
-				'singular_name' => __( 'Job' )
-			),
-		'public' => false,
-		'has_archive' => false,
-		)
-	);
-}
+		// If the scan is still running.
+		if ( $scan_status ) {
+			$to_encode['results'] = '0';
+		} else {
+			// Else return the results and clean up!
+			$scan_results = get_option( 'wpephpcompat.scan_results' );
+			$to_encode['results'] = esc_html( $scan_results );
 
-/**
- * Enqueue our JavaScript and CSS.
- *
- * @since 1.0.0
- * @return  null
- */
-function wpephpcompat_enqueue() {
-	wp_enqueue_style( 'wpephpcompat-style', plugins_url( '/src/css/style.css', __FILE__ ) );
+			$wpephpc = new \WPEPHPCompat( __DIR__ );
+			$wpephpc->clean_after_scan();
+		}
 
-	wp_enqueue_script( 'wpephpcompat-handlebars', plugins_url( '/src/js/handlebars.js', __FILE__ ), array( 'jquery' ) );
+		echo json_encode( $to_encode );
+		wp_die();
 
-	wp_enqueue_script( 'wpephpcompat-download', plugins_url( '/src/js/download.min.js', __FILE__ ) );
+	}
 
-	wp_enqueue_script( 'wpephpcompat', plugins_url( '/src/js/run.js', __FILE__ ), array('jquery', 'wpephpcompat-handlebars', 'wpephpcompat-download') );
+	/**
+	 * Create custom post type to store the directories we need to process.
+	 *
+	 * @since 1.0.0
+	 * @return  null
+	 */
+	function create_job_queue() {
+		register_post_type( 'wpephpcompat_jobs',
+			array(
+				'labels' => array(
+					'name' => __( 'Jobs' ),
+					'singular_name' => __( 'Job' )
+				),
+			'public' => false,
+			'has_archive' => false,
+			)
+		);
+	}
 
-	// Progress Bar
-	wp_enqueue_script( 'jquery-ui-progressbar' );
-	wp_enqueue_style( 'jquery-style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css' );
+	/**
+	 * Enqueue our JavaScript and CSS.
+	 *
+	 * @since 1.0.0
+	 * @action admin_enqueue_scripts
+	 * @return  null
+	 */
+	function admin_enqueue() {
+		// Styles
+		wp_enqueue_style( 'wpephpcompat-style', plugins_url( '/src/css/style.css', __FILE__ ) );
 
+		// Scripts
+		wp_enqueue_script( 'wpephpcompat-handlebars', plugins_url( '/src/js/handlebars.js', __FILE__ ), array( 'jquery' ) );
+		wp_enqueue_script( 'wpephpcompat-download', plugins_url( '/src/js/download.min.js', __FILE__ ) );
+		wp_enqueue_script( 'wpephpcompat', plugins_url( '/src/js/run.js', __FILE__ ), array('jquery', 'wpephpcompat-handlebars', 'wpephpcompat-download') );
 
-	wp_localize_script( 'wpephpcompat', 'ajax_object', array( 'ajax_url' => admin_url( 'admin-ajax.php' )) );
-}
+		// Progress Bar
+		wp_enqueue_script( 'jquery-ui-progressbar' );
+		wp_enqueue_style( 'jquery-style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css' );
 
-/**
- * Summary.
- *
- * Description.
- *
- * @since 1.0.0
- * @return [type] [description]
- */
-function wpephpcompat_create_menu() {
-	//Create Tools sub-menu.
-	$wpeallowheartbeat_settings_page = add_submenu_page( 'tools.php', 'PHP Compatibility', 'PHP Compatibility', 'administrator', __FILE__, 'wpephpcompat_settings_page' );
-}
+		// PHP to JS vars
+		wp_localize_script( 'wpephpcompat', 'ajax_object', array( 'ajax_url' => admin_url( 'admin-ajax.php' )) );
+	}
 
-/**
- * Summary.
- *
- * Description.
- *
- * @since 1.0.0
- * @return [type] [description]
- */
-function wpephpcompat_settings_page() {
-	?>
-	<div class="wrap">
-		<div style="float: left;">
-			<h2>WP Engine PHP Compatibility</h2>
+	/**
+	 * Add the settings page to the wp-admin menu.
+	 *
+	 * @since 1.0.0
+	 * @action admin_menu
+	 * @return null
+	 */
+	function create_menu() {
+		// Create Tools sub-menu.
+		$wpeallowheartbeat_settings_page = add_submenu_page( 'tools.php', 'PHP Compatibility', 'PHP Compatibility', 'administrator', __FILE__, array( self::instance(), 'settings_page' ) );
+	}
+
+	/**
+	 * Render method for the settings page.
+	 *
+	 * @since 1.0.0
+	 * @return null
+	 */
+	function settings_page() {
+		?>
+		<div class="wrap">
+			<div style="float: left;">
+				<h2>WP Engine PHP Compatibility</h2>
+			</div>
+			<div style="float: right; margin-top: 10px; text-align: right;">
+				<input type="checkbox" id="developermode" name="developermode" value="yes">Developer mode
+			</div>
+			<br><br>
+			<h3 class="title">Scan Options</h3>
+			<table class="form-table">
+				<tbody>
+					<tr>
+						<th scope="row"><label for="phptest_version">PHP Version</label></th>
+						<td>
+							<label><input type="radio" name="phptest_version" value="7.0" checked="checked"> PHP 7.0</label><br>
+							<label><input type="radio" name="phptest_version" value="5.5"> PHP 5.5</label><br>
+							<label><input type="radio" name="phptest_version" value="5.4"> PHP 5.4</label><br>
+							<label><input type="radio" name="phptest_version" value="5.3"> PHP 5.3</label>
+						</td>
+					</tr>
+					<tr>
+						<th scope="row"><label for="active_plugins">Only Active</label></th>
+						<td><label><input type="radio" name="active_plugins" value="yes" checked="checked"> Only scan active plugins and themes</label><br>
+							<label><input type="radio" name="active_plugins" value="no"> Scan all plugins and themes</label>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<p>
+				<div style="display: none;" id="wpe-progress">
+					<label for="">Progress</label>
+					<div id="progressbar"></div>
+				</div>
+
+				<!-- Area for pretty results. -->
+				<div id="standardMode"></div>
+
+				<!-- Area for developer results. -->
+				<div style="display: none;" id="developerMode">
+					<b>Test Results:</b>
+					<textarea disabled="disabled" id="testResults"></textarea>
+				</div>
+
+				<div id="footer" style="display: none;">
+					Note: Warnings are not currently an issue, but they could be in the future.<br>
+					<a id="downloadReport" href="#">Download Report</a>
+				</div>
+			</p>
+			<p>
+				<input style="float: left;" name="run" id="runButton" type="button" value="Run" class="button-primary" />
+				<div style="display:none; visibility: visible; float: none;" class="spinner"></div>
+			</p>
 		</div>
-		<div style="float: right; margin-top: 10px; text-align: right;">
-			<input type="checkbox" id="developermode" name="developermode" value="yes">Developer mode
-		</div>
-		<br><br>
-		<h3 class="title">Scan Options</h3>
-		<table class="form-table">
-			<tbody>
-				<tr>
-					<th scope="row"><label for="phptest_version">PHP Version</label></th>
-					<td>
-						<label><input type="radio" name="phptest_version" value="7.0" checked="checked"> PHP 7.0</label><br>
-						<label><input type="radio" name="phptest_version" value="5.5"> PHP 5.5</label><br>
-						<label><input type="radio" name="phptest_version" value="5.4"> PHP 5.4</label><br>
-						<label><input type="radio" name="phptest_version" value="5.3"> PHP 5.3</label>
-					</td>
-				</tr>
-				<tr>
-					<th scope="row"><label for="active_plugins">Only Active</label></th>
-					<td><label><input type="radio" name="active_plugins" value="yes" checked="checked"> Only scan active plugins and themes</label><br>
-						<label><input type="radio" name="active_plugins" value="no"> Scan all plugins and themes</label>
-					</td>
-				</tr>
-			</tbody>
-		</table>
-		<p>
-			<div style="display: none;" id="wpe-progress">
-				<label for="">Progress</label>
-				<div id="progressbar"></div>
-			</div>
 
-			<!-- Area for pretty results. -->
-			<div id="standardMode"></div>
+		<!-- Results template -->
+		<script id="result-template" type="text/x-handlebars-template">
+			<div style="border-left-color: {{#if passed}}#038103{{else}}#e74c3c{{/if}};" class="wpe-results-card">
+				<div class="inner-left">
+					{{#if passed}}<img src="<?php echo esc_url( plugins_url( '/src/images/check.png', __FILE__ ) ); ?>">{{else}}<img src="<?php echo esc_url( plugins_url( '/src/images/x.png', __FILE__ ) ); ?>">{{/if}}
+				</div>
+				<div class="inner-right">
+					<h3 style="margin: 0px;">{{plugin_name}}</h3>
+					{{#if passed}}PHP {{test_version}} compatible.{{else}}<b>Not</b> PHP {{test_version}} compatible.{{/if}}<br>
+					{{update}}<br>
+					<textarea style="display: none;">{{logs}}</textarea><a class="view-details">view details</a>
+				</div>
+				<?php $update_url = site_url( 'wp-admin/update-core.php' , 'admin' ); ?>
+				<div style="float:right;">{{#if updateAvailable}}<div class="badge wpe-update"><a href="<?php echo esc_url( $update_url ); ?>">Update Available</a></div>{{/if}}<div class="badge warnings">{{warnings}} Warnings</div><div class="badge errors">{{errors}} Errors</div></div>
+			</div>
+		</script>
+		<?php
+	}
 
-			<!-- Area for developer results. -->
-			<div style="display: none;" id="developerMode">
-				<b>Test Results:</b>
-				<textarea disabled="disabled" id="testResults"></textarea>
-			</div>
-
-			<div id="footer" style="display: none;">
-				Note: Warnings are not currently an issue, but they could be in the future.<br>
-				<a id="downloadReport" href="#">Download Report</a>
-			</div>
-		</p>
-		<p>
-			<input style="float: left;" name="run" id="runButton" type="button" value="Run" class="button-primary" />
-			<div style="display:none; visibility: visible; float: none;" class="spinner"></div>
-		</p>
-	</div>
-
-	<!-- Results template -->
-	<script id="result-template" type="text/x-handlebars-template">
-		<div style="border-left-color: {{#if passed}}#038103{{else}}#e74c3c{{/if}};" class="wpe-results-card">
-			<div class="inner-left">
-				{{#if passed}}<img src="<?php echo esc_url( plugins_url( '/src/images/check.png', __FILE__ ) ); ?>">{{else}}<img src="<?php echo esc_url( plugins_url( '/src/images/x.png', __FILE__ ) ); ?>">{{/if}}
-			</div>
-			<div class="inner-right">
-				<h3 style="margin: 0px;">{{plugin_name}}</h3>
-				{{#if passed}}PHP {{test_version}} compatible.{{else}}<b>Not</b> PHP {{test_version}} compatible.{{/if}}<br>
-				{{update}}<br>
-				<textarea style="display: none;">{{logs}}</textarea><a class="view-details">view details</a>
-			</div>
-			<?php $update_url = site_url( 'wp-admin/update-core.php' , 'admin' ); ?>
-			<div style="float:right;">{{#if updateAvailable}}<div class="badge wpe-update"><a href="<?php echo esc_url( $update_url ); ?>">Update Available</a></div>{{/if}}<div class="badge warnings">{{warnings}} Warnings</div><div class="badge errors">{{errors}} Errors</div></div>
-		</div>
-	</script>
-	<?php
 }
+// Register the WPEngine_PHPCompat instance
+WPEngine_PHPCompat::init();
+
