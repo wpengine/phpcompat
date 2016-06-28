36d17457c69b707fbe59933a606ec33fb81e5c66
Jason Stallings <jason@stallin.gs>
2016-06-28 13:54:09 -0500

    Add 7.0 scan test.

--- tests/phpunit/test_scan.php
+++ tests/phpunit/test_scan.php
@@ -21,4 +21,25 @@ PHP 5.5 compatible.
 
 		$this->assertEquals($expected_results, $results);
 	}
+
+	function test_scan_default_PHP_70() {
+		$expected_results = "Name: Twenty Sixteen
+
+PHP 7.0 compatible.
+
+";
+		$root_dir = realpath( __DIR__ . '/../../' );
+
+		$wpephpc = new \WPEPHPCompat( $root_dir );
+
+		$wpephpc->clean_after_scan();
+
+		$wpephpc->test_version = '7.0';
+
+		$wpephpc->only_active = 'yes';
+
+		$results = $wpephpc->start_test();
+
+		$this->assertEquals($expected_results, $results);
+	}
 }
