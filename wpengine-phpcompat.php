8f43f754b0991da114e3ae601d834dab9e4c4a68
Jason Cross <code@hellojason.net>
2017-01-10 15:22:56 -0600

    chore(ui update): broad strokes overhaul

--- wpengine-phpcompat.php
+++ wpengine-phpcompat.php
@@ -270,74 +270,97 @@ class WPEngine_PHPCompat {
 		$test_version = ( false !== $test_version ) ? $test_version : '7.0';
 		$only_active = ( false !== $only_active ) ? $only_active : 'yes';
 		?>
-		<div class="wrap">
-			<div style="float: left;">
-				<h2><?php esc_attr_e( 'WP Engine PHP Compatibility Checker', 'php-compatibility-checker' ); ?></h2>
-			</div>
-			<div style="float: right; margin-top: 10px; text-align: right;">
+		<div class="wrap wpe-pcc-wrap">
+			<h1><?php esc_attr_e( 'PHP Compatibility Checker' ) ?></h1>
+			<div class="wpe-pcc-main">
+				<p><?php printf( __(' Developed by <a href="%1$s">WP Engine</a>', 'php-compatibility-checker'), 'https://wpengine.com/' ); ?></p>
+				<p><?php esc_attr_e(' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'php-compatibility-checker' ); ?></p>
+				<hr>
+				<div class="wpe-pcc-scan-options">
+					<h2><?php printf( 'Scan Options' ); ?></h2>
+					<table class="form-table wpe-pcc-form-table">
+						<tbody>
+							<tr>
+								<th scope="row"><label for="phptest_version"><?php esc_attr_e( '1. What version of PHP do you want to test?', 'php-compatibility-checker' ); ?></label></th>
+								<td>
+									<?php
+									foreach ( $phpversions as $name => $version ) {
+										printf( '<label><input type="radio" name="phptest_version" value="%s" %s /> %s</label><br>', $version, checked( $test_version, $version, false ), $name );
+									} ?>
+								</td>
+							</tr>
+							<tr>
+								<th scope="row"><label for="active_plugins"><?php esc_attr_e( '2. What plugins/themes do you want to scan?', 'php-compatibility-checker' ); ?></label></th>
+								<td><label><input type="radio" name="active_plugins" value="yes" <?php checked( $only_active, 'yes', true ); ?> /> <?php esc_attr_e( 'Only scan active plugins and themes', 'php-compatibility-checker' ); ?></label><br>
+									<label><input type="radio" name="active_plugins" value="no" <?php checked( $only_active, 'no', true ); ?> /> <?php esc_attr_e( 'Scan all plugins and themes', 'php-compatibility-checker' ); ?></label>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+
+					<div class="wpe-pcc-run-scan">
+						<input style="float: left;" name="run" id="runButton" type="button" value="<?php esc_attr_e( 'Scan', 'php-compatibility-checker' ); ?>" class="button-primary" />
+						<div class="wpe-tooltip">
+							<input style="float: left; margin-left: 5px;" name="run" id="cleanupButton" type="button" value="<?php esc_attr_e( 'Clear results', 'php-compatibility-checker' ); ?>" class="wpe-pcc-clear-results" />
+							<span class="wpe-tooltiptext"><?php esc_attr_e( 'This will remove all database options related to this plugin, but it will not stop a scan in progress. If you\'d like to completely remove all data wait for the scan to finish before hitting this button.' ); ?></span>
+						</div>
+					</div> <!-- /wpe-pcc-run-scan -->
+
+				</div> <!-- /wpe-pcc-scan-options -->
+				<hr>
+				<div class="wpe-pcc-results">
+					<h2><?php printf( 'Scan Results <span style="display:none; visibility:visible; float:none; padding-left: 10px;" class="spinner wpe-pcc-spinner"></span> '); ?></h2>
+
+					<!-- Progress bar -->
+					<div style="display: none;" id="wpe-progress">
+						<p><?php printf( '<strong>Scan progress</strong> - <span id="wpe-progress-count"></span> <span id="wpe-progress-active"></span>', 'php-compatibility-checker' ); ?></p>
+						<div id="progressbar"></div>
+					</div>
+
+					<!-- Area for pretty results. -->
+					<div id="standardMode"></div>
+
+					<!-- Area for developer results. -->
+					<div style="display: none;" id="developerMode">
+						<b><?php esc_attr_e( 'Test Results:', 'php-compatibility-checker' ); ?></b>
+						<textarea readonly="readonly" id="testResults"></textarea>
+					</div>
+
+					<p><?php printf( __( 'This plugin relies on WP-Cron to scan files in the background. The scan will get stuck if the site&#39s WP-Cron is not running correctly. Please <a href="%1$s">see the FAQ</a> for more information.', 'php-compatibility-checker' ), 'https://wordpress.org/plugins/php-compatibility-checker/faq/' ); ?></p>
+					<p><?php printf( __( 'Report false positives <a href="%1$s">on our GitHub repo</a>.', 'php-compatibility-checker' ), 'https://github.com/wpengine/phpcompat/wiki/Results' ); ?></p>
+				</div> <!-- /wpe-pcc-results -->
+			</div> <!-- /wpe-pcc-main -->
+			<div class="wpe-pcc-aside">
+				<p class="wpe-pcc-logo"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 268.3 51"><g fill="#40BAC8"><path d="M17.4 51h16.4V38.6l-4-4h-8.5l-3.9 4zM38.6 17.3l-3.9 3.9v8.6l3.9 3.9h12.5V17.3zM33.8 0H17.4v12.5l3.9 3.9h8.5l4-3.9zM51.1 51V38.6l-3.9-4H34.7V51zM4 0L.1 3.9v12.5h16.4V0zM34.7 0v12.5l3.9 3.9h12.5V0zM25.6 27.9c-1.3 0-2.3-1.1-2.3-2.3 0-1.3 1.1-2.3 2.3-2.3 1.3 0 2.3 1.1 2.3 2.3 0 1.2-1 2.3-2.3 2.3zM16.5 17.3H.1v16.4h12.4l4-3.9zM16.5 38.6l-4-4H.1V51h12.4l4-3.9z"/></g><g fill="#162A33"><path d="M86.2 38.6c-.3 0-.4-.1-.5-.4l-4.1-14.5h-.1l-4.1 14.5c-.1.3-.2.4-.5.4h-4.8c-.3 0-.4-.1-.5-.4l-7-25.2c0-.2 0-.4.3-.4h6.3c.3 0 .5.2.5.4L75 28.1h.1l4-15.1c.1-.3.2-.4.5-.4h3.9c.3 0 .4.1.5.4l4.2 15.1h.1L91.5 13c0-.2.2-.4.5-.4h6.3c.2 0 .3.2.3.4l-7 25.2c-.1.3-.2.4-.5.4h-4.9zM103.6 38.6c-.2 0-.4-.2-.4-.4V13c0-.2.2-.4.4-.4H114c6.3 0 9.6 3.6 9.6 8.6s-3.3 8.7-9.6 8.7h-3.8c-.2 0-.2.1-.2.2v8c0 .2-.2.4-.4.4h-6zm13.3-17.3c0-1.8-1.2-2.9-3.3-2.9h-3.4c-.2 0-.2.1-.2.2V24c0 .2.1.2.2.2h3.4c2.1 0 3.3-1.2 3.3-2.9zM132.5 32.2c-.5-1.4-.7-3.1-.7-6.5 0-3.3.3-5.1.7-6.5 1.3-4.1 4.5-6.2 8.6-6.2 4.2 0 7.3 2.1 8.6 6.2.5 1.4.7 3 .7 6.1 0 .3-.2.5-.6.5h-16.3c-.2 0-.3.2-.3.4 0 2.7.2 4.2.6 5.5 1.2 3.7 3.9 5.3 7.5 5.3 3.4 0 5.9-1.5 7.4-3.5.2-.3.5-.3.7-.1l.3.3c.3.2.3.5.1.7-1.7 2.4-4.6 4.1-8.4 4.1-4.5 0-7.5-2.1-8.9-6.3zm16.2-7.8c.2 0 .3-.1.3-.3 0-1.7-.2-3.1-.6-4.3-1.1-3.5-3.7-5.3-7.2-5.3s-6.1 1.7-7.2 5.3c-.4 1.2-.6 2.5-.6 4.3 0 .2.1.3.3.3h15zM173.6 38c-.3 0-.5-.2-.5-.5V22.9c0-5.8-2.4-8.3-7.1-8.3-4.1 0-7.5 2.8-7.5 7.6v15.4c0 .3-.2.5-.5.5h-.5c-.3 0-.5-.2-.5-.5V14.2c0-.3.2-.5.5-.5h.5c.3 0 .5.2.5.5v3.4h.1c1.2-2.8 4-4.5 7.5-4.5 5.5 0 8.6 3.1 8.6 9.4v15c0 .3-.2.5-.5.5h-.6zM182 44.3c-.2-.3-.2-.6.1-.7l.4-.3c.3-.2.5-.1.7.2 1.4 1.8 3.5 2.9 6.5 2.9 4.6 0 7.6-2.3 7.6-8.3V34h-.1c-1.2 2.7-3.3 4.6-7.6 4.6-4.1 0-6.9-2.2-8.1-5.8-.6-1.7-.8-3.9-.8-6.9 0-3 .3-5.2.8-6.9 1.2-3.6 4-5.8 8.1-5.8 4.3 0 6.4 1.9 7.6 4.6h.1v-3.5c0-.3.2-.5.5-.5h.5c.3 0 .5.2.5.5v23.9c0 6.7-3.6 9.7-9.2 9.7-3.5-.1-6.4-1.7-7.6-3.6zm14.6-12.1c.5-1.5.7-3.3.7-6.4 0-3-.2-4.9-.7-6.4-1.2-3.6-3.8-4.9-6.8-4.9-3.3 0-5.7 1.6-6.7 4.8-.5 1.5-.8 3.6-.8 6.4 0 2.8.3 4.9.8 6.4 1.1 3.2 3.4 4.8 6.7 4.8 3 .2 5.7-1.1 6.8-4.7zM207.2 6.1c-.3 0-.5-.2-.5-.5v-2c0-.3.2-.5.5-.5h1.2c.3 0 .5.2.5.5v2.1c0 .3-.2.5-.5.5h-1.2zm.4 31.9c-.3 0-.5-.2-.5-.5V14.2c0-.3.2-.5.5-.5h.5c.3 0 .5.2.5.5v23.3c0 .3-.2.5-.5.5h-.5zM233.5 38c-.3 0-.5-.2-.5-.5V22.9c0-5.8-2.4-8.3-7.1-8.3-4.1 0-7.5 2.8-7.5 7.6v15.4c0 .3-.2.5-.5.5h-.5c-.3 0-.5-.2-.5-.5V14.2c0-.3.2-.5.5-.5h.5c.3 0 .5.2.5.5v3.4h.1c1.2-2.8 4-4.5 7.5-4.5 5.5 0 8.6 3.1 8.6 9.4v15c0 .3-.2.5-.5.5h-.6zM241.4 32.2c-.5-1.4-.7-3.1-.7-6.5 0-3.3.3-5.1.7-6.5 1.3-4.1 4.5-6.2 8.6-6.2 4.2 0 7.3 2.1 8.6 6.2.5 1.4.7 3 .7 6.1 0 .3-.2.5-.6.5h-16.3c-.2 0-.3.2-.3.4 0 2.7.2 4.2.6 5.5 1.2 3.7 3.9 5.3 7.5 5.3 3.4 0 5.9-1.5 7.4-3.5.2-.3.5-.3.7-.1l.3.3c.3.2.3.5.1.7-1.7 2.4-4.6 4.1-8.4 4.1-4.5 0-7.6-2.1-8.9-6.3zm16.1-7.8c.2 0 .3-.1.3-.3 0-1.7-.2-3.1-.6-4.3-1.1-3.5-3.7-5.3-7.2-5.3s-6.1 1.7-7.2 5.3c-.4 1.2-.6 2.5-.6 4.3 0 .2.1.3.3.3h15z"/></g><g><path fill="#162A33" d="M262.3 16.1c0-1.7 1.3-3 3-3s3 1.3 3 3-1.3 3-3 3-3-1.3-3-3zm5.5 0c0-1.5-1.1-2.5-2.5-2.5-1.5 0-2.5 1.1-2.5 2.5 0 1.5 1.1 2.5 2.5 2.5s2.5-1 2.5-2.5zm-3.5 1.7c-.1 0-.1 0-.1-.1v-3.1c0-.1 0-.1.1-.1h1.2c.7 0 1.1.4 1.1 1 0 .4-.2.8-.7.9l.7 1.3c.1.1 0 .2-.1.2h-.3c-.1 0-.1-.1-.2-.1l-.7-1.3h-.7v1.2c0 .1-.1.1-.1.1h-.2zm1.8-2.4c0-.3-.2-.5-.6-.5h-.8v1h.8c.4 0 .6-.2.6-.5z"/></g></svg></p>
+				<div class="wpe-pcc-aside-content">
+					<h2>Launch this site in a PHP7 hosting environment and double your site speed!</h2>
+					<p>Easily test your site on a PHP7 server or launch your new PHP7 site now!</p>
+					<a class="wpe-pcc-button" href="#">Get PHP7 Hosting!</a>
+
+					<hr>
+
+					<h2>Need help making this site PHP7 compatible?</h2>
+					<p class="wpe-pcc-attention"><strong>Attention:</strong> Not all errors are show-stoppers. <a href="#">Test this site in PHP7</a> to see if just works.</p>
+					<div class="wpe-pcc-dev-helper">
+						<p class="title"><strong>Get help from WP Engine partners</strong></p>
+						<img src="http://placehold.it/233x105" />
+						<p>We partner with the brightest agency minds that are dedicated to delivering enterprise grade solutions in WordPress.</p>
+					</div> <!-- /wpe-pcc-dev-helper -->
+
+					<div class="wpe-pcc-dev-helper">
+						<p class="title"><strong>Get a fast quote from Codeable</strong></p>
+						<img src="http://placehold.it/233x105" />
+						<p>Here is some text about this thing right here that could be a few lines long.</p>
+					</div> <!-- /wpe-pcc-dev-helper -->
+
+				</div> <!-- /wpe-pcc-aside-content -->
 				<input type="checkbox" id="developermode" name="developermode" value="yes" /><?php esc_attr_e( 'Developer mode', 'php-compatibility-checker' ); ?>
-			</div>
-			<div class="clear">
-				<p><?php esc_attr_e( 'This plugin will lint theme and plugin code inside your WordPress file system and give you back a report of compatibility issues for you to fix. Compatibility issues are categorized into errors and warnings and will list the file and line number of the offending code, as well as the info about why that line of code is incompatible with the chosen version of PHP. The plugin will also suggest updates to themes and plugins, as a new version may offer compatible code.', 'php-compatibility-checker' ); ?></p>
-								<p><b><?php esc_attr_e( 'This plugin does not execute your theme and plugin code, as such this plugin cannot detect runtime compatibility issues.', 'php-compatibility-checker' ); ?></b></p>
+			</div> <!-- /wpe-pcc-aside -->
+		</div> <!-- /wpe-pcc-wrap -->
+		<!-- // end new markup -->
 
 
-<p><?php printf( __( 'Please note that linting code is not perfect. This plugin cannot detect unused code paths that might be used for backwards compatibility, and thus might show false positives. You can <a href="%1$s">report false positives on our GitHub repo</a>.', 'php-compatibility-checker' ), 'https://github.com/wpengine/phpcompat/wiki/Results' ); ?></p>
 
-<p><?php printf( __( 'This plugin relies on WP-Cron to scan files in the background. The scan will get stuck if the site&#39s WP-Cron is not running correctly. Please <a href="%1$s">see the FAQ</a> for more information.', 'php-compatibility-checker' ), 'https://wordpress.org/plugins/php-compatibility-checker/faq/' ); ?></p>
-			</div>
-			<hr>
-			<h3 class="title clear"><?php esc_attr_e( 'Scan Options', 'php-compatibility-checker' ); ?></h3>
-			<table class="form-table">
-				<tbody>
-					<tr>
-						<th scope="row"><label for="phptest_version"><?php esc_attr_e( 'PHP Version', 'php-compatibility-checker' ); ?></label></th>
-						<td>
-							<?php
-							foreach ( $phpversions as $name => $version ) {
-								printf( '<label><input type="radio" name="phptest_version" value="%s" %s /> %s</label><br>', $version, checked( $test_version, $version, false ), $name );
-							}
-							?>
-						</td>
-					</tr>
-					<tr>
-						<th scope="row"><label for="active_plugins"><?php esc_attr_e( 'Only Active', 'php-compatibility-checker' ); ?></label></th>
-						<td><label><input type="radio" name="active_plugins" value="yes" <?php checked( $only_active, 'yes', true ); ?> /> <?php esc_attr_e( 'Only scan active plugins and themes', 'php-compatibility-checker' ); ?></label><br>
-							<label><input type="radio" name="active_plugins" value="no" <?php checked( $only_active, 'no', true ); ?> /> <?php esc_attr_e( 'Scan all plugins and themes', 'php-compatibility-checker' ); ?></label>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-			<hr />
-			<p>
-				<div style="display: none;" id="wpe-progress">
-					<label for=""><?php esc_attr_e( 'Progress', 'php-compatibility-checker' ); ?></label>
-					<div id="progressbar"></div>
-					<div id="wpe-progress-count"></div>
-					<div id="wpe-progress-active"></div>
-				</div>
-
-				<!-- Area for pretty results. -->
-				<div id="standardMode"></div>
-
-				<!-- Area for developer results. -->
-				<div style="display: none;" id="developerMode">
-					<b><?php esc_attr_e( 'Test Results:', 'php-compatibility-checker' ); ?></b>
-					<textarea readonly="readonly" id="testResults"></textarea>
-				</div>
-
-				<div id="footer" style="display: none;">
-					<?php esc_attr_e( 'Note: PHP Warnings will not cause errors, but could cause compatibility issues with future PHP versions, and could spam your PHP logs.', 'php-compatibility-checker' ); ?><br>
-					<a id="downloadReport" href="#"><?php esc_attr_e( 'Download Report', 'php-compatibility-checker' ); ?></a>
-				</div>
-			</p>
-			<p>
-				<input style="float: left;" name="run" id="runButton" type="button" value="<?php esc_attr_e( 'Run', 'php-compatibility-checker' ); ?>" class="button-primary" />
-				<div class="wpe-tooltip"><input style="float: left; margin-left: 5px;" name="run" id="cleanupButton" type="button" value="<?php esc_attr_e( 'Clean up', 'php-compatibility-checker' ); ?>" class="button" />
-				<span class="wpe-tooltiptext">This will remove all database options related to this plugin, but it will not stop a scan in progress. If you'd like to completly remove all data wait for the scan to finish before hitting this button.</span></div>
-				<div style="display:none; visibility: visible; float: left;" class="spinner"></div>
-			</p>
-		</div>
 
 		<!-- Results template -->
 		<script id="result-template" type="text/x-handlebars-template">
