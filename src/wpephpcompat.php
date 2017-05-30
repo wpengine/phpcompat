a5992779093b9c997c076d5128527724ce3bda7e
Jason Cosper <boogah@gmail.com>
2017-05-30 13:40:06 -0700

    Adding Give to whitelist

--- src/wpephpcompat.php
+++ src/wpephpcompat.php
@@ -84,6 +84,7 @@ class WPEPHPCompat {
 		'*/comment-mail/*' => '7.0', // https://wordpress.org/support/topic/false-positive-comment-mail/
 		'*/social-networks-auto-poster-facebook-twitter-g/*' => '7.0', // https://wordpress.org/plugins/social-networks-auto-poster-facebook-twitter-g/
 		'*/mailpoet/*' => '7.0', // https://wordpress.org/support/topic/false-positive-mailpoet-3-not-compatible-with-php7/
+		'*/give/*' => '7.0', // https://github.com/wpengine/phpcompat/issues/148
 	);
 
 	/**
