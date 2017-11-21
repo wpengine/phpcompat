d6620d5847dc1fd76b84262ceec07bb972f266f5
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-11-21 13:38:30 -0600

    Fix tests setup to work on old PHP versions and be more performant.

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -1,12 +1,24 @@
 <?php
-$_tests_dir = getenv('WP_TESTS_DIR');
-if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
-require_once $_tests_dir . '/includes/functions.php';
-// Include our plugin.
-function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../../wpengine-phpcompat.php';
-}
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-// Bootstrap WordPress.
-require $_tests_dir . '/includes/bootstrap.php';
+// Disable xdebug backtrace.
+if ( function_exists( 'xdebug_disable' ) ) {
+	xdebug_disable();
+}
+
+if ( false !== getenv( 'WP_PLUGIN_DIR' ) ) {
+	define( 'WP_PLUGIN_DIR', getenv( 'WP_PLUGIN_DIR' ) );
+}
+
+$GLOBALS['wp_tests_options'] = array(
+	'active_plugins' => array( basename( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/wpengine-phpcompat.php' ),
+);
+
+if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
+	$test_root = getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit';
+} elseif ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
+	$test_root = '/tmp/wordpress-tests-lib';
+} else {
+	$test_root = '../../../../../../../tests/phpunit';
+}
+
+require $test_root . '/includes/bootstrap.php';
