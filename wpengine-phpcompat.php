0bb86fa352d740766ad1e246cac8b96b648ffc7c
Steven K Word <stevenword@gmail.com>
2018-01-18 11:27:42 -0600

    Adds better version detection for enqueuing scripts

--- wpengine-phpcompat.php
+++ wpengine-phpcompat.php
@@ -10,7 +10,7 @@
  * Plugin URI:  https://wpengine.com
  * Description: Make sure your plugins and themes are compatible with newer PHP versions.
  * Author:      WP Engine
- * Version:     1.4.4
+ * Version:     1.4.5
  * Author URI:  https://wpengine.com
  * Text Domain: php-compatibility-checker
  */
