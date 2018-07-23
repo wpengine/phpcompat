7c0ee6063586398a072d3879df21ecf8c2ac7758
Jason Stallings <jason@stallin.gs>
2018-07-23 12:48:08 -0500

    Consistant spacing.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -d "build" ]; then
-  svn co https://plugins.svn.wordpress.org/php-compatibility-checker build
+	svn co https://plugins.svn.wordpress.org/php-compatibility-checker build
 else
 	pushd build
 	svn up
@@ -10,19 +10,19 @@ fi
 composer install --no-dev
 rsync \
 	-avuP \
-  --delete \
-  --exclude="Tests" \
-  --include='wpengine-phpcompat.php' \
-  --include='uninstall.php' \
-  --include='load-files.php' \
-  --include='readme.txt' \
-  --include='src' \
-  --include="src/**" \
-  --include='vendor' \
-  --include='vendor/**' \
-  --include='php52' \
-  --include='php52/**' \
-  --exclude="*" \
+	--delete \
+	--exclude="Tests" \
+	--include='wpengine-phpcompat.php' \
+	--include='uninstall.php' \
+	--include='load-files.php' \
+	--include='readme.txt' \
+	--include='src' \
+	--include="src/**" \
+	--include='vendor' \
+	--include='vendor/**' \
+	--include='php52' \
+	--include='php52/**' \
+	--exclude="*" \
 	./ ./build/trunk
 
 pushd ./build/trunk
