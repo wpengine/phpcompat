cc9ee2b18472321c5bdef7fe4f478fa062983513
Mike Angstadt <mike.angstadt@wpengine.com>
2018-09-03 08:42:23 -0500

    Foo

--- tests/phpunit/test_scan.php
+++ tests/phpunit/test_scan.php
@@ -1,5 +1,7 @@
 <?php
-class TestScan extends WP_UnitTestCase {
+use PHPUnit\Framework\TestCase as TestCase;
+
+class TestScan extends TestCase {
 
 	function test_scan_default_PHP_55() {
 		$root_dir = realpath( dirname( __FILE__ ) . '/../../' );
