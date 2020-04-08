45dd54b8d6f5e58e676e99173ee3e05b054de005
Anthony Burchell <antpb@Anthonys-MacBook-Pro.local>
2020-04-08 10:57:06 -0500

    adjusts test to new bootstrap WIP

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -1,41 +1,27 @@
 <?php
-
-// Disable xdebug backtrace.
-if ( function_exists( 'xdebug_disable' ) ) {
-	xdebug_disable();
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package Generated_Plugin
+ */
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
-
-if ( false !== getenv( 'WP_PLUGIN_DIR' ) ) {
-	define( 'WP_PLUGIN_DIR', getenv( 'WP_PLUGIN_DIR' ) );
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	exit( 1 );
 }
-
-$GLOBALS['wp_tests_options'] = array(
-	'active_plugins' => array( basename( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/wpengine-phpcompat.php' ),
-);
-
-if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	$test_root = getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit';
-} elseif ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
-	$test_root = '/tmp/wordpress-tests-lib';
-} else {
-	$test_root = '../../../../../../../tests/phpunit';
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	require dirname( dirname( dirname( __FILE__ ) ) ) . '/wpengine-phpcompat.php';
 }
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
 
-if ( file_exists( $test_root . '/includes/functions.php' ) ) {
-	require_once $test_root . '/includes/functions.php';
-	function _manually_load_plugin() {
-		require dirname( __FILE__ ) . '/../../wpengine-phpcompat.php';
-	}
 
-	tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-}
-
-// Core started including their own phpunit6-compat, so we should use that if we can.
-if ( ! file_exists( $test_root . '/includes/phpunit6/compat.php' ) ) {
-	// WordPress versions before 4.8 will be incompatible with newer PHPUnit versions.
-	if ( version_compare( getenv( 'WP_VERSION' ), '4.8', '<' ) && class_exists( 'PHPUnit\Runner\Version' ) ) {
-		require_once dirname( __FILE__ ) . '/phpunit6-compat.php';
-	}
-}
-
-require $test_root . '/includes/bootstrap.php';
