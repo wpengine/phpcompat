56a526d1f3ec4d77bcf1b2ac3a8e364ff539c22e
Anthony Burchell <antpb@Anthonys-MacBook-Pro.local>
2020-04-15 16:30:23 -0500

    removes 5.2 support

--- load-files.php
+++ load-files.php
@@ -17,9 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 function wpephpcompat_load_files() {
 	require_once dirname( __FILE__ ) . '/src/wpephpcompat.php';
 
-	if ( version_compare( phpversion(), '5.3', '<' ) ) {
-		$autoload_file = dirname( __FILE__ ) . '/php52/vendor/autoload_52.php';
-	} else {
+	if ( version_compare( phpversion(), '5.2', '>' ) ) {
 		$autoload_file = dirname( __FILE__ ) . '/vendor/autoload.php';
 	}
 
