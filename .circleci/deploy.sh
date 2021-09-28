93be81afc3271f983f121587e5bdd796c1428851
Anthony Burchell <anthony.burchell@wpengine.com>
2021-09-28 13:08:07 -0500

    svn commit on deploy success

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -73,9 +73,6 @@ svn cp "trunk" "tags/$RELEASE_TAG"
 svn status
 
 echo "Committing files..."
-# svn commit -m "Release version $RELEASE_TAG." --no-auth-cache --non-interactive --username "$SVN_USERNAME" --password "$SVN_PASSWORD"
-mkdir /tmp/zip
-cd /tmp/zip
-tar -czvf /tmp/zip/1.5.2.tar.gz /tmp/artifacts/tags/1.5.2
+svn commit -m "Release version $RELEASE_TAG." --no-auth-cache --non-interactive --username "$SVN_USERNAME" --password "$SVN_PASSWORD"
 
 echo "Plugin version $RELEASE_TAG deployed."
