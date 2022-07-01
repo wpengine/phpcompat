7052edc581a02cf9cc166e490a9898ecf491f677
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-07-01 18:05:54 +0300

    Cleanup and comments

--- src/js/include/render.js
+++ src/js/include/render.js
@@ -1,7 +1,6 @@
 import Mustache from "mustache";
 import $ from "jquery";
 import compareVersions from "compare-versions";
-const { __ } = wp.i18n;
 
 export function initResults(itemsToScan, activeOnly) {
   const container = $("#wpe_pcc_results");
@@ -46,34 +45,43 @@ export function initResults(itemsToScan, activeOnly) {
 export function updateResult(response, job) {
   const resultItem = $(`#${job.type}_${job.slug}`);
   const template = $("#result-template").html().toString();
+
+  // Data to render the template.
   const view = {
     ...job,
   };
 
   const report = response.reports?.phpcs_phpcompatibilitywp?.report;
 
+  // Success if no errors.
   view.status =
     report.totals.errors === 0 && report.totals.warnings === 0
       ? "success"
       : "error";
 
+  // Create index for PHP Versions.
   const phpVersions = Object.keys(report.versions).sort(compareVersions);
+
   view.php = [];
   view.reports = [];
+
   phpVersions.forEach((phpVersion) => {
     view.php.push({
       phpversion: phpVersion,
       passed: report.compatible.includes(phpVersion),
-      error: report.incompatible.includes(phpVersion),
     });
 
+	// Extract all error messages from incompatible PHP versions.
     if (report.incompatible.includes(phpVersion)) {
       const messages = [];
       const files = report.versions[phpVersion].files;
+
+	  // In each file of the plugin, add error messages.
       Object.keys(files).forEach((file) => {
         if (files[file].messages.length) {
           files[file].messages.forEach((message) => {
-            messages.push(
+            // Compile plain text with source file, line, error code and error text.
+			messages.push(
               `${file}:${message.line}\n${message.source}\n${message.message}`
             );
           });
@@ -102,8 +110,8 @@ export function updateResultFailure(response, job) {
   const view = {
     ...job,
     status: "error",
-	custom_error: true,
-	response_status: response.status
+    custom_error: true,
+    response_status: response.status,
   };
 
   const output = Mustache.render(template, view);
